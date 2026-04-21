b7a1eb04c049d2df06920c12a220ea2b6e373155
Gaston <ggastlino@gmail.com>
2026-04-21 05:44:21 +0200

    feat(admin): update DashboardController for EasyAdmin integration and enhance menu items

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -7,28 +7,30 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+
+use App\Controller\Admin\Authentification\UserCrudController;
+
+use App\Controller\Admin\Profile\ProfileCrudController;
+use App\Controller\Admin\Profile\HousingTypeCrudController;
+
+use App\Controller\Admin\Animals\AnimalCrudController;
+use App\Controller\Admin\Animals\AnimalTagCrudController;
+use App\Controller\Admin\Animals\SpeciesCrudController;
+use App\Controller\Admin\Animals\TagCrudController;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // return $this->redirectToRoute('admin_user_index');
+        $url = $adminUrlGenerator
+            ->setController(AnimalCrudController::class)
+            ->generateUrl();
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -39,7 +41,18 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkTo(SomeCrudController::class, 'The Label', 'fas fa-list');
+
+        yield MenuItem::section('Users');
+        yield MenuItem::linkTo(UserCrudController::class, 'Users', 'fas fa-users');
+
+        yield MenuItem::section('Profiles');
+        yield MenuItem::linkTo(ProfileCrudController::class, 'Profiles', 'fas fa-user');
+        yield MenuItem::linkTo(HousingTypeCrudController::class, 'Housing Types', 'fas fa-home');
+
+        yield MenuItem::section('Animals');
+        yield MenuItem::linkTo(AnimalCrudController::class, 'Animals', 'fas fa-paw');
+        yield MenuItem::linkTo(AnimalTagCrudController::class, 'Animal Tags', 'fas fa-tags');
+        yield MenuItem::linkTo(SpeciesCrudController::class, 'Species', 'fas fa-dna');
+        yield MenuItem::linkTo(TagCrudController::class, 'Tags', 'fas fa-tag');
     }
 }
