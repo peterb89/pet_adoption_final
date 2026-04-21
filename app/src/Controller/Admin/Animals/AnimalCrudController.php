4afeba8f7f0b4af1a5e8c5ef3dcd9237819970e1
Gaston <ggastlino@gmail.com>
2026-04-21 05:46:37 +0200

    feat(admin): add AnimalCrudController for managing animals in EasyAdmin

---
+++ app/src/Controller/Admin/Animals/AnimalCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin\Animals;
+
+use App\Entity\Animals\Animal;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AnimalCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Animal::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            AssociationField::new('species'),
+            TextField::new('breed'),
+            IntegerField::new('age'),
+            TextField::new('location'),
+            TextField::new('status'),
+        ];
+    }
+}
