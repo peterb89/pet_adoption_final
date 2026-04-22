80bc437aa9f850316af783ac471960fa61a93a4f
peterb89 <bozsipeter@gmail.com>
2026-04-22 17:05:50 +0200

    feat: Finished GitHub-style Profile UI with Edit and Show pages

---
+++ app/src/Controller/ProfileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Profile\Profile;
+use App\Form\ProfileType;
+use App\Repository\Profile\ProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route; // Modern Route elérési út
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile_show')]
+    public function show(ProfileRepository $profileRepository): Response
+    {
+        // Megkeressük az első profilt a teszteléshez
+        $profile = $profileRepository->findOneBy([]);
+
+        return $this->render('profile/show.html.twig', [
+            'profile' => $profile,
+        ]);
+    }
+
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, ProfileRepository $profileRepository, EntityManagerInterface $entityManager): Response
+    {
+        $profile = $profileRepository->findOneBy([]);
+        
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profile_show');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+            'profile' => $profile,
+        ]);
+    }
+}
