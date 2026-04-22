eccae24819a629f103b70989869b561f7874e4bf
peterb89 <bozsipeter@gmail.com>
2026-04-22 17:51:27 +0200

    Final cleanup of Profile UI before Sprint 3 meeting

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace App\Controller\Admin;
 
-use App\Controller\Admin\ProfileCrudController; // Ez kell az átirányításhoz
+use App\Controller\Admin\ProfileCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator; // Ez kell a generátorhoz
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
@@ -14,7 +15,6 @@ class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        // Tech Lead Fix: Az üdvözlő oldal helyett rögtön a Profilokra ugrunk, így látszani fog a menü!
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($adminUrlGenerator->setController(ProfileCrudController::class)->generateUrl());
     }
@@ -28,11 +28,16 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
 
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToRoute('Users List', 'fas fa-users', 'admin_user_index');
+
         yield MenuItem::section('Profiles');
-        yield MenuItem::linkToRoute('Profiles', 'fa fa-user', 'admin_profile_index');
-        yield MenuItem::linkToRoute('Housing Types', 'fa fa-building', 'admin_housing_type_index');
+        yield MenuItem::linkToRoute('Profiles', 'fas fa-user', 'admin_profile_index');
+        yield MenuItem::linkToRoute('Housing Types', 'fas fa-house-user', 'admin_housing_type_index');
 
         yield MenuItem::section('Animals');
-        yield MenuItem::linkToRoute('Animals List', 'fa fa-paw', 'admin_animal_index');
+        yield MenuItem::linkToRoute('Animals List', 'fas fa-paw', 'admin_animal_index');
+        yield MenuItem::linkToRoute('Species', 'fas fa-dna', 'admin_species_index');
+        yield MenuItem::linkToRoute('Tags', 'fas fa-tag', 'admin_tag_index');
     }
 }
