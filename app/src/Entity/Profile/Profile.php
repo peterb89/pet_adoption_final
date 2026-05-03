e040d54ceb027322b03f52a367df53782fcee5f3
Gaston <ggastlino@gmail.com>
2026-05-03 19:04:18 +0200

    fix: refactor Profile entity to improve structure and add user relationship

--- app/src/Entity/Profile/Profile.php
+++ app/src/Entity/Profile/Profile.php
@@ -2,78 +2,122 @@
 
 namespace App\Entity\Profile;
 
+use App\Entity\Authentification\User;
 use App\Repository\Profile\ProfileRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProfileRepository::class)]
 class Profile
 {
-    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column] private ?int $id = null;
-    #[ORM\Column(length: 255)] private ?string $firstName = null;
-    #[ORM\Column(length: 255)] private ?string $lastName = null;
-    #[ORM\Column(length: 20, nullable: true)] private ?string $phoneNumber = null;
-    #[ORM\Column(length: 255)] private ?string $city = null;
-    #[ORM\Column(length: 255)] private ?string $address = null;
-    #[ORM\ManyToOne(inversedBy: 'profiles')] private ?HousingType $housingType = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne]
+    #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
+    private ?User $user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $phoneNumber = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\ManyToOne(inversedBy: 'profiles')]
+    private ?HousingType $housingType = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
     public function getFirstName(): ?string
     {
         return $this->firstName;
     }
+
     public function setFirstName(string $f): self
     {
         $this->firstName = $f;
         return $this;
     }
+
     public function getLastName(): ?string
     {
         return $this->lastName;
     }
+
     public function setLastName(string $l): self
     {
         $this->lastName = $l;
         return $this;
     }
+
     public function getPhoneNumber(): ?string
     {
         return $this->phoneNumber;
     }
+
     public function setPhoneNumber(?string $p): self
     {
         $this->phoneNumber = $p;
         return $this;
     }
+
     public function getCity(): ?string
     {
         return $this->city;
     }
+
     public function setCity(string $c): self
     {
         $this->city = $c;
         return $this;
     }
+
     public function getAddress(): ?string
     {
         return $this->address;
     }
+
     public function setAddress(string $a): self
     {
         $this->address = $a;
         return $this;
     }
+
     public function getHousingType(): ?HousingType
     {
         return $this->housingType;
     }
+
     public function setHousingType(?HousingType $h): self
     {
         $this->housingType = $h;
         return $this;
     }
+
     public function __toString(): string
     {
         return trim(sprintf('%s %s', $this->firstName ?? '', $this->lastName ?? ''));
